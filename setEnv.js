54652e2e5ed5eee9de8ffba1cf39336abfb866ae
ZanTech12 <dontechiel@gmail.com>
2026-05-10 18:39:13 +0100

    fix: replace setEnv with dotenv

--- setEnv.js
+++ setEnv.js
@@ -5,7 +5,7 @@ if (!process.env.JWT_SECRET) process.env.JWT_SECRET = "my_super_secret_key_chang
 if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
 if (!process.env.EMAIL_USER) process.env.EMAIL_USER = "dontechiel@gmail.com";
 if (!process.env.EMAIL_PASS) process.env.EMAIL_PASS = "lfwczhfiymxgdcdp";
-if (!process.env.ADMIN_EMAIL) process.env.ADMIN_EMAIL = "dontechiel@gmail.com";
+if (!process.env.ADMIN_EMAIL) process.env.ADMIN_EMAIL = "bishopafoo@gmail.com";
 if (!process.env.FRONTEND_URL) process.env.FRONTEND_URL = "http://localhost:3000";
 
 // REMOVE the PORT line completely! Let Render handle it automatically.
